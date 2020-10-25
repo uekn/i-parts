72186aadf286964146216a3a2f272c3c5e430d52
uekn <kusuba1181@gmail.com>
2020-10-25 14:36:36 +0900

    モデル・コントローラにacts_as_taggable_on対応のための記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
     validates :content, presence: true
 
     has_many_attached :images
+
+    acts_as_taggable_on :tags
+    acts_as_taggable_on :skills, :interests
 end
